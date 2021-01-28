39ca3aacf5c369653b94feacd86e13ec6078b62c
AndyKQ <andyquang1610@icloud.com>
2021-01-27 20:48:45 -0500

    add error check in init
    renderer window init

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -14,16 +14,28 @@ int main(int argc, char* argv[])
 	}
 	else
 	{
-		cout << "Problem innitializing SDL! Problem report :" << SDL_GetError << endl;
+		cout << "Problem innitializing SDL!" << endl;
 		return 1; // Immediately exits program
 	}
 
 	p_window = SDL_CreateWindow("HEllo world", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
 		1024, 1024, NULL);
-	p_renderer = SDL_CreateRenderer(p_window, -1, NULL);
-	// Initialization done
+	if (p_window == nullptr)
+	{
+		cout << "Problem innitializing window!" << endl;
+			return 2 ; // Immediately exits program
+	}
+	else
+	{
+		p_renderer = SDL_CreateRenderer(p_window, -1, SDL_RENDERER_ACCELERATED);
+		if (p_renderer == nullptr)
+		{
+			cout << "Problem innitializing render!" << endl;
+			return 3; // Immediately exits program
+		}
+	}
 
-	SDL_SetRenderDrawColor(p_renderer, 255, 124, 0, 255); //0-255 for eache color channel
+	SDL_SetRenderDrawColor(p_renderer, 255, 128, 64, 255); //0-255 for eache color channel
 	SDL_RenderClear(p_renderer); // 'paint over' window with draw color
 	SDL_RenderPresent(p_renderer); // refreshes window
 	system("pause");
